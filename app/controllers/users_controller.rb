f17e1d8a4ea03888b62bcaa06b8c3811b8c57b6f
ansmr <nishim0712@gmail.com>
2024-06-08 08:06:19 +0900

    fix: 課題 7b, 8b, 9b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,35 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+
+    # 今日の投稿数取得
+    @book_count_today = @books.created_today.count
+    # 前日の投稿数取得
+    @book_count_yesterday = @books.created_yesterday.count
+    # 今週の投稿数取得
+    @book_count_this_week = @books.created_this_week.count
+    # 前週の投稿数取得
+    @book_count_last_week = @books.created_last_week.count
+
+    # １週間分の投稿数取得
+    @book_count_2day_ago = @books.created_2day_ago.count
+    @book_count_3day_ago = @books.created_3day_ago.count
+    @book_count_4day_ago = @books.created_4day_ago.count
+    @book_count_5day_ago = @books.created_5day_ago.count
+    @book_count_6day_ago = @books.created_6day_ago.count
+  end
+
+  # 非同期検索用
+  def search
+    @user = User.find(params[:user_id])
+    @books = @user.books
+    if params[:created_at] == ""
+      @search_book = "日付を選択してください"
+    else
+      create_at = params[:created_at]
+      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count
+    end
+    render 'search_result'
   end
 
   def index
